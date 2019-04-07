646d6d5cb78fd644ee5782a8441c6abbb994e697
Jarne <jarne@users.noreply.github.com>
2019-04-07 13:57:26 +0200

    Moved to GitHub / updated dependencies

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # GetWord
 A website to generate passwords
 
-[![CircleCI](https://circleci.com/bb/thejarne/getword.svg?style=svg&circle-token=b3f67057a906a2eec784dc51433d36ca06d9a43d)](https://circleci.com/bb/thejarne/getword)
+[![CircleCI](https://circleci.com/gh/jarne/GetWord.svg?style=svg&circle-token=)](https://circleci.com/gh/jarne/GetWord)
--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,49 @@
 /**
- * Created by jarne on 28.07.17.
+ * GetWord | gulp file
  */
 
-var gulp = require("gulp");
-var concat = require("gulp-concat");
-var cleanCss = require("gulp-clean-css");
-var uglify = require("gulp-uglify");
+const gulp = require("gulp");
+const concat = require("gulp-concat");
+const cleanCss = require("gulp-clean-css");
+const uglify = require("gulp-uglify");
 
-gulp.task("jquery", function() {
+gulp.task("jquery", () => {
     return gulp.src("bower_components/jquery/dist/jquery.min.js")
         .pipe(gulp.dest("assets/dest/js"));
 });
 
-gulp.task("bootstrap-css", function() {
+gulp.task("bootstrap-css", () => {
     return gulp.src("bower_components/bootstrap/dist/css/bootstrap.min.css")
         .pipe(gulp.dest("assets/dest/css"));
 });
 
-gulp.task("bootstrap-js", function() {
+gulp.task("bootstrap-js", () => {
     return gulp.src("bower_components/bootstrap/dist/js/bootstrap.min.js")
         .pipe(gulp.dest("assets/dest/js"));
 });
 
-gulp.task("index-css", function() {
+gulp.task("index-css", () => {
     return gulp.src("assets/src/css/index.css")
         .pipe(concat("index.min.css"))
         .pipe(cleanCss())
         .pipe(gulp.dest("assets/dest/css"));
 });
 
-gulp.task("index-js", function() {
+gulp.task("index-js", () => {
     return gulp.src("assets/src/js/index.js")
         .pipe(concat("index.min.js"))
         .pipe(uglify())
         .pipe(gulp.dest("assets/dest/js"));
 });
 
-gulp.task("watch", function() {
-    gulp.watch("bower_components/jquery/jquery.min.js", ["jquery"]);
+gulp.task("watch", () => {
+    gulp.watch("bower_components/jquery/jquery.min.js", gulp.parallel("jquery"));
 
-    gulp.watch("bower_components/bootstrap/dist/css/bootstrap.min.css", ["bootstrap-css"]);
-    gulp.watch("bower_components/bootstrap/dist/js/bootstrap.min.js", ["bootstrap-js"]);
+    gulp.watch("bower_components/bootstrap/dist/css/bootstrap.min.css", gulp.parallel("bootstrap-css"));
+    gulp.watch("bower_components/bootstrap/dist/js/bootstrap.min.js", gulp.parallel("bootstrap-js"));
 
-    gulp.watch("assets/src/css/index.css", ["index-css"]);
-    gulp.watch("assets/src/js/index.js", ["index-js"]);
+    gulp.watch("assets/src/css/index.css", gulp.parallel("index-css"));
+    gulp.watch("assets/src/js/index.js", gulp.parallel("index-js"));
 });
 
-gulp.task("default", ["jquery", "bootstrap-css", "bootstrap-js", "index-css", "index-js"]);
+gulp.task("default", gulp.parallel("jquery", "bootstrap-css", "bootstrap-js", "index-css", "index-js"));
